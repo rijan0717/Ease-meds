0d9c2dc3dc3b7f03e272593d73310296f725aa17
Rijan Pariyar | RentalBux <rijan.p@rentalbux.com>
2026-05-14 10:15:22 +0100

    fixes on css that was not linked

--- header.php
+++ header.php
@@ -13,7 +13,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/ease-meds/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 
